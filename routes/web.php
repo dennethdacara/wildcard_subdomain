54edb6d6382396bf9e5457f3e3fc3b7474de76ff
Denneth Dacara <denneth@gmail.com>
2018-10-09 14:10:09 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,17 @@
 <?php
 
-// Route::group (['domain' => '{account} .domain.com'], function () 
-// {
-//   Route::get ('user/{id}', function ($account, $id) {
-//   }); 
-// });
-
-Route::group(['domain' => 'wildcard'], function() {
-    Route::any('/', function() {
-        return 'My own domain';
-    }); 
+Route::any('/', function() {
+    return 'My own domain';
 }); 
 
-Route::group(['domain' => '{username}.wildcard'], function() {
-    Route::any('/', function($username) {
-        return 'You visit your account: '. $username; 
-    });
-});
+// Route::group(['domain' => 'wildcard'], function() {
+//     Route::any('/', function() {
+//         return 'My own domain';
+//     }); 
+// }); 
+
+// Route::group(['domain' => '{username}.wildcard'], function() {
+//     Route::any('/', function($username) {
+//         return 'You visit your account: '. $username; 
+//     });
+// });

b1929c3d1168431063c0eb839eebc4f6aa730cec
Denneth Dacara <denneth@gmail.com>
2018-10-09 14:56:44 +0800

    test

--- routes/web.php
+++ routes/web.php
@@ -4,13 +4,13 @@
 //     return 'My own domain';
 // }); 
 
-Route::group(['domain' => '18.220.78.40'], function() {
+Route::group(['domain' => 'projred.webuffsolutions.com'], function() {
     Route::any('/', function() {
         return 'My own domain';
     }); 
 }); 
 
-Route::group(['domain' => '{username}.18.220.78.40'], function() {
+Route::group(['domain' => '{username}.projred.webuffsolutions.com'], function() {
     Route::any('/', function($username) {
         return 'You visit your account: '. $username; 
     });
